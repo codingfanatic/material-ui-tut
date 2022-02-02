2d74a20635f88747be591abb7d91197645b62a17
codingfanatic <codingfanatic@github.com>
2022-02-01 19:06:42 -0500

    Finished Lesson 1 (with some of lesson 3) OOps

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Typography, Button } from '@material-ui/core'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button type="submit">Submit</Button>
     </div>
   )
 }
